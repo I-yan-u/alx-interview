7e09bf85d3d0c27dc1951391312039c8558c5ce7
Iyanu Ajimobi <iyanuajimbi12@gmail.com>
2023-10-19 23:42:59 +0100

    sorted the output

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,49 +1,58 @@
 #!/usr/bin/python3
+"""Reads from standard input and computes metrics.
+
+After every ten lines or the input of a keyboard interruption (CTRL + C),
+prints the following statistics:
+    - Total file size up to that point.
+    - Count of read status codes up to that point.
 """
-Write a script that reads stdin line by line and computes metrics
-"""
-from sys import stdin
 
 
-def count_occurrences(input_list, target):
+def print_stats(size, status_codes):
+    """Print accumulated metrics.
+
+    Args:
+        size (int): The accumulated read file size.
+        status_codes (dict): The accumulated count of status codes.
+    """
+    print("File size: {}".format(size))
+    for key in sorted(status_codes):
+        print("{}: {}".format(key, status_codes[key]))
+
+if __name__ == "__main__":
+    import sys
+
+    size = 0
+    status_codes = {}
+    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
     count = 0
-    for item in input_list:
-        if item == target:
-            count += 1
-    return count
-
-
-def stat_dict(lst, hash_table):
-    for item in lst:
-        count = count_occurrences(lst, item)
-        hash_table[item] = count
-    return hash_table
-
-
-def stats():
-    status_codes = []
-    stat_count = {}
-    file_size = 0
 
     try:
-        for line in stdin:
-            pattern = line.split()
-            status_code = pattern[-2]
-            status_codes.append(status_code)
-            file_size += int(pattern[-1])
+        for line in sys.stdin:
+            if count == 10:
+                print_stats(size, status_codes)
+                count = 1
+            else:
+                count += 1
 
-            if len(status_codes) % 10 == 0:
-                stat_count = stat_dict(status_codes, stat_count)
-                print("File size: {}".format(file_size))
-                for k in sorted(stat_count):
-                    print("{}: {}".format(k, stat_count[k]))
+            line = line.split()
+
+            try:
+                size += int(line[-1])
+            except (IndexError, ValueError):
+                pass
+
+            try:
+                if line[-2] in valid_codes:
+                    if status_codes.get(line[-2], -1) == -1:
+                        status_codes[line[-2]] = 1
+                    else:
+                        status_codes[line[-2]] += 1
+            except IndexError:
+                pass
+
+        print_stats(size, status_codes)
 
     except KeyboardInterrupt:
-        stat_count = stat_dict(status_codes, stat_count)
-        print("File size: {}".format(file_size))
-        for k in sorted(stat_count):
-            print("{}: {}".format(k, stat_count[k]))
-
-
-if __name__ == '__main__':
-    stats()
+        print_stats(size, status_codes)
+        raise
