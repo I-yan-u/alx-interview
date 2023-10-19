42e12ee3f1fa897a10442af80a323121fd01e109
Iyanu Ajimobi <iyanuajimbi12@gmail.com>
2023-10-19 22:46:00 +0100

    refactored

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,63 +1,58 @@
 #!/usr/bin/python3
+"""Reads from standard input and computes metrics.
+
+After every ten lines or the input of a keyboard interruption (CTRL + C),
+prints the following statistics:
+    - Total file size up to that point.
+    - Count of read status codes up to that point.
 """
-Write a script that reads stdin line by line and computes metrics
-"""
-from sys import stdin
-import re
 
 
-def count_occurrences(input_list, target):
+def print_stats(size, status_codes):
+    """Print accumulated metrics.
+
+    Args:
+        size (int): The accumulated read file size.
+        status_codes (dict): The accumulated count of status codes.
+    """
+    print("File size: {}".format(size))
+    for key in sorted(status_codes):
+        print("{}: {}".format(key, status_codes[key]))
+
+if __name__ == "__main__":
+    import sys
+
+    size = 0
+    status_codes = {}
+    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
     count = 0
-    for item in input_list:
-        if item == target:
-            count += 1
-    return count
 
-
-def stat_dict(lst, hash_table):
-    for item in lst:
-        count = count_occurrences(lst, item)
-        hash_table[item] = count
-    return hash_table
-
-
-def stats():
-    requests = []
-    status_codes = []
-    stat_count = {}
-    file_size = 0
-    reg = r'(\d+\.\d+\.\d+\.\d+) - \[([^\]]+)\] "GET ([^"]+)" (\d+) (\d+)'
     try:
-        for line in stdin:
-            log_entry = line
-            requests.append(line)
-
-            pattern = reg
-
-            matches = re.match(pattern, log_entry)
-
-            if matches:
-                ip_address = matches.group(1)
-                date = matches.group(2)
-                status_code = matches.group(4)
-                status_codes.append(status_code)
-                file_size += int(matches.group(5))
-
-                if len(requests) % 10 == 0:
-                    status_codes.sort()
-                    stat_count = stat_dict(status_codes, stat_count)
-                    print("File size: {}".format(file_size))
-                    for k, v in stat_count.items():
-                        print("{}: {}".format(k, v))
+        for line in sys.stdin:
+            if count == 10:
+                print_stats(size, status_codes)
+                count = 1
             else:
-                continue
+                count += 1
+
+            line = line.split()
+
+            try:
+                size += int(line[-1])
+            except (IndexError, ValueError):
+                pass
+
+            try:
+                if line[-2] in valid_codes:
+                    if status_codes.get(line[-2], -1) == -1:
+                        status_codes[line[-2]] = 1
+                    else:
+                        status_codes[line[-2]] += 1
+            except IndexError:
+                pass
+
+        print_stats(size, status_codes)
+
     except KeyboardInterrupt:
-        status_codes.sort()
-        stat_count = stat_dict(status_codes, stat_count)
-        print("File size: {}".format(file_size))
-        for k, v in stat_count.items():
-            print("{}: {}".format(k, v))
-
-
-if __name__ == '__main__':
-    stats()
+        print_stats(size, status_codes)
+        raise
