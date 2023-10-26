ff0e2e180702e41a5841c7dcb69c6273ba219f69
Iyanu Ajimobi <iyanuajimbi12@gmail.com>
2023-10-26 23:00:07 +0100

    validate utf-8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -5,39 +5,31 @@ Validates a given dataset to be UTF-8 encoded
 from typing import List
 
 
-def bin_check(integer: int) -> int:
-    """
-    convert to binary
-    """
-    bin = []
-    a = integer
-    count = 0
-    final = False
-    while a//2 != 0:
-        a //= 2
-        count += 1
-    count += 1
-
-    for _ in range(count):
-        bin.append(integer % 2)
-        integer //= 2
-
-    if len(bin) > 8:
-        bin.pop()
-
-    for bit in bin:
-        if bit == 1:
-            final = True
-    return final
+def get_leading_set_bits(num):
+    """returns the number of leading set bits (1)"""
+    set_bits = 0
+    helper = 1 << 7
+    while helper & num:
+        set_bits += 1
+        helper = helper >> 1
+    return set_bits
 
 
-def validUTF8(data: List[int]) -> bool:
-    """
-    UTF-8 validation
-    """
-    if not data:
-        return False
-    truthy = 1
-    for intg in data:
-        truthy *= bin_check(intg)
-    return bool(truthy)
+def validUTF8(data):
+    """determines if a given data set represents a valid UTF-8 encoding"""
+    bits_count = 0
+    for i in range(len(data)):
+        if bits_count == 0:
+            bits_count = get_leading_set_bits(data[i])
+            '''1-byte (format: 0xxxxxxx)'''
+            if bits_count == 0:
+                continue
+            '''a character in UTF-8 can be 1 to 4 bytes long'''
+            if bits_count == 1 or bits_count > 4:
+                return False
+        else:
+            '''checks if current byte has format 10xxxxxx'''
+            if not (data[i] & (1 << 7) and not (data[i] & (1 << 6))):
+                return False
+        bits_count -= 1
+    return bits_count == 0
